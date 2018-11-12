8a29b1fb8e6efbfa81f921b1c19c986dce6f732e
Melissa Real <melissa.real@hotmail.com>
2018-11-12 17:28:23 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,14 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+  @@pirates = []
+  def initialize
+    @name = name
+    @weight = weight
+    @height = height
+    @@pirates << self
+  end
+
+  def all
+    @@pirates.all
+  end 
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  @@ships = []
+  def initialize
+    @name = name
+    @type = type
+    @booty = booty
+    @@ships << self
+  end
+
+  def all
+    @@ships.all
+  end
+
+  def clear
+    @@ships.clear 
+  end
+
+
 end
